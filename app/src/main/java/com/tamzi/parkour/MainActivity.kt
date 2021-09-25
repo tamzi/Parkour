8024286f424bb6dc582c5489baa0128bf87493c6
tamzi <tamrefrank@gmail.com>
2021-09-25 21:59:56 +0300

    Initial commit

---
+++ app/src/main/java/com/tamzi/parkour/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.tamzi.parkour
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.tamzi.parkour.ui.theme.ParkourTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ParkourTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ParkourTheme {
+        Greeting("Hilted")
+    }
+}
